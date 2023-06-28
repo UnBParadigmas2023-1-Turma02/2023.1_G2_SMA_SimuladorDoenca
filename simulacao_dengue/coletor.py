22a16812037b4f480012059af2bf9f72d81eedcc
Júlia Farias Sousa <julia@MacBook-Pro-de-Julia.local>
2023-06-28 18:07:08 -0300

    Adicionando os tipos de grupo de pessoas que serão analisados

---
+++ simulacao_dengue/coletor.py
@@ -0,0 +1,21 @@
+#coleta informacoes da situacao de cada grupo de pessoas
+
+class Status:
+    @staticmethod
+    def numero_infectados(model):
+        return sum
+    @staticmethod
+    def numero_graves(model):
+        return sum
+    @staticmethod
+    def numero_curados(model):
+        return sum
+    @staticmethod
+    def numero_mortos(model):
+        return sum
+    @staticmethod
+    def mortalidade(model):
+        mortos = Status.numero_mortos(model)
+        infectados = Status.numero_infectados(model)  
+        curados = Status.numero_curados(model)
+        return mortos / (mortos + infectados + curados)
