6d7f40add8bc3be4e98903bc1c2c29dc0e05503e
LamequeFernandes <lamequesao@gmail.com>
2023-07-01 19:13:30 -0300

    logica para randomizar a probabilidade de morte das pessoas

--- simulacao_dengue/Agentes/pessoa.py
+++ simulacao_dengue/Agentes/pessoa.py
@@ -1,4 +1,5 @@
 from mesa import Agent
+import random
 
 class Pessoa(Agent):
 
@@ -8,12 +9,22 @@ class Pessoa(Agent):
         self.timesInfected = 0
 
     def increase_counter_infected(self):
-        if self.timesInfected < 3:
+        if self.timesInfected == 0:
             self.timesInfected += 1
+            self.calculate_probability_die(0.15)
+        elif self.timesInfected == 1:
+            self.timesInfected += 1
+            self.calculate_probability_die(0.35)
+        elif self.timesInfected == 2:
+            self.timesInfected += 1
+            self.calculate_probability_die(0.75)
         else:
+            ... # vive para sempre ;)
+
+    def calculate_probability_die(self, probability_die):
+        if random.random() < probability_die:
             self.model.grid._remove_agent(self.pos, self)
             self.model.schedule.remove(self)
 
     def is_sting(self):
         self.isInfected = True 
-
